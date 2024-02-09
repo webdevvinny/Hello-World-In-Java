62dd477524658c873d1a573cce1ffd6ebdcb1267
Web Dev Vinny <whiteangel@duck.com>
2024-02-09 09:04:00 +0300

    A Simple Java GUI Application

---
+++ src/Vehicle.java
@@ -0,0 +1,23 @@
+
+package k;
+final class Vehicle {
+    final String brand = "Ford";        // Vehicle attribute
+    public void honk() {                    // Vehicle method
+        System.out.println("The vehicle is hooting: ---> Tut, tut!");
+    }
+}
+
+class Car extends Vehicle {
+    private final String modelName = "Mustang";    // Car attribute
+    public static void main(String[] args) {
+
+        // Create a myCar object
+        Car myCar = new Car();
+
+        // Call the honk() method (from the Vehicle class) on the myCar object
+        myCar.honk();
+
+        // Display the value of the brand attribute (from the Vehicle class) and the value of the modelName from the Car class
+        System.out.println(myCar.brand + " " + myCar.modelName);
+    }
+}
